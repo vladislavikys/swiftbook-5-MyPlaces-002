09ff732da192959a7a1927c917ea215eb68091d0
vladislavikys <zarya.vlad@yandex.ru>
2023-02-14 10:29:58 +0300

    Initial Commit

---
+++ swiftbook-5-MyPlaces-002/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftbook-5-MyPlaces-002
+//
+//  Created by Влад on 14.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
